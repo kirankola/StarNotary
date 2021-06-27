f417ddae82b2de204df13ccd090f5c35a426b5b7
kirankola <kiran.kola@sportsjun.com>
2021-06-28 01:22:03 +0530

    fixed code in index.js

--- app/src/index.js
+++ app/src/index.js
@@ -10,10 +10,12 @@ const App = {
     const { web3 } = this;
     try {
       // get contract instance
-      const networkId = web3.version.network;
+      const networkId = await web3.eth.getChainId();
+      console.log(networkId);
       const deployedNetwork = starNotaryArtifact.networks[networkId];
-      this.meta = web3.eth.contract(starNotaryArtifact.abi).at(deployedNetwork.address);
-      const accounts = web3.eth.accounts;
+      console.log(deployedNetwork);
+      this.meta = new web3.eth.Contract(starNotaryArtifact.abi,deployedNetwork.address);
+      const accounts = await web3.eth.getAccounts();
       this.account = accounts[0];
     } catch (error) {
       console.error("Could not connect to contract or chain.");
@@ -40,28 +42,31 @@ const App = {
   },
 
   createStar: async function() {
-    const name = document.getElementById("starName").value;
-    const id = document.getElementById("starId").value;
-
-    await this.meta.createStar(name, id, {from: this.account}, function(err,result) {
-      if(err){
-        console.log(err);
-      }
-    });
-    App.setStatus("New Star Owner is " + this.account + ".");
+    try{
+      const name = document.getElementById("starName").value;
+      const id = document.getElementById("starId").value;
+      console.log(this.account);
+      await this.meta.methods.createStar(name, id).send({from: this.account});
+    
+          App.setStatus("New Star Owner is " + this.account + ".");
+        }
+          catch(error){
+            console.error(error.message);
+          }
+    
   },
   
   // Implement Task 4 Modify the front end of the DAPP
   lookUp: async function (){
-    const id = Number(document.getElementById("lookid").value);
-    await this.meta.lookUptokenIdToStarInfo(id, function(err,result) {
-      if(!err){
-        App.setStatus(`Star with ID ${id} is ${result}`);
-      }else{
-        console.log(err);
-        App.setStatus(`Error fetching star name.`);
+    try {
+      const id = Number(document.getElementById("lookid").value);
+    const result = await this.meta.methods.lookUptokenIdToStarInfo(id).call() 
+    App.setStatus(`Star with ID ${id} is ${result}`);
       }
-    });
+      catch(error){
+      console.error(error.message);
+    }
+    
     
   }
 
